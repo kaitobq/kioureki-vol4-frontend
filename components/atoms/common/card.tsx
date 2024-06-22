52fb4c7af674d0df99a5b6988aa62f71fd60ce73
kaitobq <kaitobq520@gmail.com>
2024-06-22 18:27:21 +0900

    update: classname props

--- components/atoms/common/card.tsx
+++ components/atoms/common/card.tsx
@@ -10,8 +10,8 @@ export function Card({ children, className, ...rest }: CardProps) {
   return (
     <div
       className={clsx(
-        className,
-        "p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8"
+        "p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8",
+        className
       )}
       {...rest}
     >
