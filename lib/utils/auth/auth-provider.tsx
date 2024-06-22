41518e446ab1c991f227e111acdce7b5d5e1158f
kaitobq <kaitobq520@gmail.com>
2024-06-22 18:21:50 +0900

    feat: authProvider

--- lib/utils/auth/auth-provider.tsx
+++ lib/utils/auth/auth-provider.tsx
@@ -1,3 +1,12 @@
-export function AuthProvider() {
-  return <div>AuthProvider</div>;
+"use client";
+
+import { useUser } from "@/lib/hooks/auth/use-user";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const { user, loading, error, refetch: fetchUser } = useUser();
+  return <>{children}</>;
 }
