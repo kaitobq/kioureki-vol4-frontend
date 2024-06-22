4a7cc6989e1235471b7dc4945d03d63344cdbf38
kaitobq <kaitobq520@gmail.com>
2024-06-22 18:20:05 +0900

    feat: page dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,10 @@
-import { DashboardHeader } from "@/components/organism/dashboard/dashbord-header";
-import { Dropdown } from "@/components/organism/dashboard/dropdown";
+import Dashboard from "@/components/organism/dashboard/dashboard";
+import { AuthProvider } from "@/lib/utils/auth/auth-provider";
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p className="p-5">kafjaaknvavn;</p>
-
-      <DashboardHeader />
-      <p className="p-5">kafjaaknvavn;</p>
-    </div>
+    <AuthProvider>
+      <Dashboard />
+    </AuthProvider>
   );
 }
