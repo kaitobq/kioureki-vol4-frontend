11a952f12e8693e88fd71a10aa4c6dd8733931ad
kaitobq <kaitobq520@gmail.com>
2024-06-22 18:20:54 +0900

    feat: component dashboardBody

---
+++ components/molecules/dashboard/dashboard-body.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@/components/atoms/common/skeleton";
+
+export default function DashboardBody() {
+  return (
+    <div>
+      <Skeleton className=" w-[100px] h-[10px]" />
+    </div>
+  );
+}
