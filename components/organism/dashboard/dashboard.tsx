6e05bd707ff6cb0f63ca7ece650f3405f78230f8
kaitobq <kaitobq520@gmail.com>
2024-06-22 18:21:26 +0900

    component dashboard

---
+++ components/organism/dashboard/dashboard.tsx
@@ -0,0 +1,11 @@
+import DashboardBody from "@/components/molecules/dashboard/dashboard-body";
+import { DashboardHeader } from "../../molecules/dashboard/dashboard-header";
+
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <DashboardHeader />
+      <DashboardBody />
+    </div>
+  );
+}
