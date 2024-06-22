83420d6f94161cbb1475343db569a8cda5d6f649
kaitobq <kaitobq520@gmail.com>
2024-06-22 18:31:33 +0900

    feat: skeletonTable

---
+++ components/atoms/dashboard/skeleton-table.tsx
@@ -0,0 +1,30 @@
+import { Skeleton } from "../common/skeleton";
+
+export function SkeletonTable() {
+  return (
+    <>
+      <tr className="*:py-2 *:px-5">
+        <td>
+          <Skeleton className="h-7 w-20 rounded-md" />
+        </td>
+        <td>
+          <Skeleton className="h-7 w-20 rounded-md" />
+        </td>
+        <td>
+          <Skeleton className="h-7 w-20 rounded-md" />
+        </td>
+      </tr>
+      <tr className="*:py-2 *:px-5">
+        <td>
+          <Skeleton className="h-7 w-20 rounded-md" />
+        </td>
+        <td>
+          <Skeleton className="h-7 w-20 rounded-md" />
+        </td>
+        <td>
+          <Skeleton className="h-7 w-20 rounded-md" />
+        </td>
+      </tr>
+    </>
+  );
+}
