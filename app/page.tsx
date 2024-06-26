921b39010ec5cc26c26f761e93559dd5d231e357
kaitobq <kaitobq520@gmail.com>
2024-06-26 22:25:43 +0900

    feat: selectorコンポーネントを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { Button } from "@/components/atoms/common/button";
+import { Selector } from "@/components/atoms/common/selector";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+const schema = z.object({
+  country: z.string().min(1, { message: "国を選択してください" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -11,11 +20,35 @@ export default function Home() {
     }, 5000);
     setLoading(false);
   }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  const countries = [
+    { value: "", label: "国を選択してください" },
+    { value: "jp", label: "日本" },
+    { value: "us", label: "アメリカ" },
+    { value: "uk", label: "イギリス" },
+  ];
   return (
     <div>
       kioureki
       {loading ? <Button loading /> : <Button>submit</Button>}
       <Button loading />
+      <Selector
+        id="country"
+        options={countries}
+        {...register("country")}
+        className="w-[200px]"
+      />
     </div>
   );
 }
---
+++ components/atoms/common/selector.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import { ComponentProps, forwardRef } from "react";
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+interface SelectorProps extends Omit<ComponentProps<"select">, "ref"> {
+  options: Option[];
+  className?: string;
+}
+
+function SelectorComponent(
+  { options, className, ...rest }: SelectorProps,
+  ref: React.Ref<HTMLSelectElement>
+) {
+  return (
+    <select
+      ref={ref}
+      className={clsx(
+        "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-pointer",
+        className
+      )}
+      {...rest}
+    >
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export const Selector = forwardRef<HTMLSelectElement, SelectorProps>(
+  SelectorComponent
+);
