8c9b9fc32cbd99cf6c000b2b81863728d7fa845b
kaitobq <kaitobq520@gmail.com>
2024-06-26 22:12:15 +0900

    feat: inputコンポーネントを作成

---
+++ components/atoms/common/input.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import React, { ComponentProps, forwardRef } from "react";
+
+interface InputProps extends ComponentProps<"input"> {
+  className?: string;
+}
+
+function InputComponent(props: InputProps, ref: React.Ref<HTMLInputElement>) {
+  const { className, ...rest } = props;
+  return (
+    <input
+      ref={ref}
+      className={clsx(
+        "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5",
+        className
+      )}
+      {...rest}
+    />
+  );
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(InputComponent);
