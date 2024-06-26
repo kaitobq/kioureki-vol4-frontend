8b45c9b192cf0967c1e683c6efbcfa48ab60a0a6
kaitobq <kaitobq520@gmail.com>
2024-06-26 14:25:22 +0900

    エラー時に/authに遷移

--- lib/hooks/auth/use-user.ts
+++ lib/hooks/auth/use-user.ts
@@ -4,17 +4,22 @@ import { useState, useEffect } from "react";
 import apiClient from "@/lib/utils/api/apiclient";
 import { User } from "@/types/common/user";
 import { getCookie } from "../cookie/use-cookie";
+import { useRouter } from "next/navigation";
 
 export function useUser() {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   const fetchUser = async () => {
+    console.log("fetchUser");
+    setLoading(true);
     try {
       const token = await getCookie("token");
       if (!token) {
         setError("No token found");
+        router.push("/auth");
         setLoading(false);
         return;
       }
@@ -24,15 +29,17 @@ export function useUser() {
       });
 
       setUser(response.data);
+      setLoading(false);
     } catch (error: any) {
       setError(error.message || "Failed to fetch user data");
-    } finally {
-      setLoading(false);
+      console.error(error);
+      router.push("/auth");
     }
   };
 
   useEffect(() => {
     fetchUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { user, loading, error, refetch: fetchUser };
