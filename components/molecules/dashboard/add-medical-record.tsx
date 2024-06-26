73ec0d0e4661a1df21e0db53d8655b7570850c0d
kaitobq <kaitobq520@gmail.com>
2024-06-26 14:26:12 +0900

    medical-record追加用コンポーネントを作成

--- components/molecules/dashboard/add-medical-record.tsx
+++ components/molecules/dashboard/add-medical-record.tsx
@@ -1,3 +1,17 @@
+import { AddMedicalRecordDialog } from "@/components/atoms/dashboard/add-medical-record-dialog";
+import { DialogTrigger } from "@/components/atoms/dashboard/dialog-trigger";
+import { useCallback, useRef, useState } from "react";
+
 export function AddMedicalRecord() {
-  return <div>AddMedicalRecord</div>;
+  const [open, setOpen] = useState(false);
+  const toggleDialog = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <DialogTrigger toggleDialog={toggleDialog} />
+      <AddMedicalRecordDialog open={open} toggleDialog={toggleDialog} />
+    </div>
+  );
 }
