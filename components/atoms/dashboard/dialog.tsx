8239a867ed7557f40d4f5a9b511bb7b4b26a3ccb
kaitobq <kaitobq520@gmail.com>
2024-06-27 13:55:35 +0900

    dialog修正

--- components/atoms/dashboard/dialog.tsx
+++ components/atoms/dashboard/dialog.tsx
@@ -1,13 +1,22 @@
+import clsx from "clsx";
 import { ComponentProps, useEffect, useRef } from "react";
 
 interface DialogProps extends ComponentProps<"dialog"> {
   open: boolean;
   toggleDialog: () => void;
+  className?: string;
   children: React.ReactNode;
 }
 
-export function Dialog({ open, toggleDialog, children, ...rest }: DialogProps) {
+export function Dialog({
+  open,
+  toggleDialog,
+  className,
+  children,
+  ...rest
+}: DialogProps) {
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const backRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const dialog = dialogRef.current;
@@ -20,8 +29,8 @@ export function Dialog({ open, toggleDialog, children, ...rest }: DialogProps) {
     }
   }, [open]);
 
-  const handleBackdropClick = (e: React.MouseEvent<HTMLDialogElement>) => {
-    if (e.target === dialogRef.current) {
+  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === backRef.current) {
       toggleDialog();
     }
   };
@@ -29,11 +38,14 @@ export function Dialog({ open, toggleDialog, children, ...rest }: DialogProps) {
     <dialog
       ref={dialogRef}
       className="bg-transparent p-0 m-0 max-w-full max-h-full"
-      onClick={handleBackdropClick}
       {...rest}
     >
-      <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
-        <div className="bg-white rounded-lg p-6 relative">
+      <div
+        ref={backRef}
+        className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center"
+        onClick={handleBackdropClick}
+      >
+        <div className={clsx("bg-white rounded-lg p-6 relative", className)}>
           <button
             className="absolute top-0 right-2 text-gray-500 hover:text-gray-700 text-2xl"
             onClick={toggleDialog}
@@ -46,3 +58,5 @@ export function Dialog({ open, toggleDialog, children, ...rest }: DialogProps) {
     </dialog>
   );
 }
+
+Dialog.displayName = "Dialog";
